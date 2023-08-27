07798d64bffd7181b9c404d32af0ba14d087c6f5
xiayu <404638507@qq.com>
2023-08-27 14:56:04 +0800

    init

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: "Lilyanlucy's blog",
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
